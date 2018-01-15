25fcbdc88cb9eb62b0e64e80199d03e5ffcd0f8c
Tianyi Zhao <mikechiu@foxmail.com>
2018-01-15 22:44:45 +0000

    modify framework, add more projects

--- main.js
+++ main.js
@@ -11,6 +11,13 @@ function loadResume(){
 function updateMenuTag(page){
     document.getElementById("menu-" + page).setAttribute("class", "menu-content-selected");
 }
-var loadThisPage = function(){
-    console.log("NOT IMPLEMENTED");
+
+function contactEmail(){
+    window.open('mailto:tz1@andrew.cmu.edu', '_self')
+}
+function contactGithub(){
+    window.open('https://github.com/ZiuTinyat', '_blank')
+}
+function contactFacebook(){
+    window.open('https://www.facebook.com/profile.php?id=100006528554451', '_blank')
 }
