d795ffb763941802d78f06fcf99958dcbaf57ada
Tianyi Zhao <mikechiu@foxmail.com>
2018-01-15 20:30:49 +0000

    for github: change all php to html

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 console.log("main.js");
 function openProj(proj){
-    window.open("../projects/" + proj + ".php");
+    window.open("../projects/" + proj + ".html");
 }
 function loadPage(page){
-    window.open("../pages/" + page + ".php", "_self");
+    window.open("../pages/" + page + ".html", "_self");
 }
 function loadResume(){
     window.open('../files/CV-Tianyi Zhao.pdf', '_blank')
 }
 function updateMenuTag(page){
     document.getElementById("menu-" + page).setAttribute("class", "menu-content-selected");
+}
+var loadThisPage = function(){
+    console.log("NOT IMPLEMENTED");
 }
--- pages/about.php
+++
@@ -1,8 +0,0 @@
-<?php echo file_get_contents("../framework_1.html"); ?>
-<script>updateMenuTag("about");</script>
-
-<h1>
-    About me page under construction
-</h1>
-
-<?php echo file_get_contents("../framework_2.html"); ?>
