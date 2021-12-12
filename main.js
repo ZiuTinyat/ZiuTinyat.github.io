3704da32f6f687ba54ea9df15bd74807e1e80bcf
Tianyi Zhao <ziutinyat@gmail.com>
2021-12-12 00:56:32 -0800

    add things to portfolio page
    a lot to do yet!

--- main.js
+++ main.js
@@ -15,19 +15,58 @@ function updateMenuTag(page){
 }
 
 function contactEmail(){
-    window.open('mailto:ziutinyat@gmail.com', '_self')
+    window.open('mailto:ziutinyat@gmail.com', '_self');
 }
 function contactGithub(){
-    window.open('https://github.com/ZiuTinyat', '_blank')
+    window.open('https://github.com/ZiuTinyat', '_blank');
 }
 function contactFacebook(){
-    window.open('https://www.facebook.com/profile.php?id=100006528554451', '_blank')
+    window.open('https://www.facebook.com/profile.php?id=100006528554451', '_blank');
 }
 function contactLinkedin(){
-    window.open('https://www.linkedin.com/in/tianyi-zhao-018ba9154/', '_blank')
+    window.open('https://www.linkedin.com/in/tianyi-zhao-018ba9154/', '_blank');
 }
 
-function filterClassType(class, type){
-	document.getElementsByClassName(class);
-	
+class FilterGroup{
+	constructor(rootElement, filterClass){
+		this.root = rootElement;
+		this.filterClass = filterClass;
+
+		var childrenCollection = this.root.getElementsByClassName("filter");
+		this.children = {};
+		for (var i = 0; i < childrenCollection.length; i++){
+			var e = childrenCollection[i];
+			this.children[e.getAttribute("name")] = e;
+		}
+	}
+
+	applyFilterElement(childElement){
+		this.deselectAllChildren();
+		childElement.classList.add("filter-selected");
+		this.filterClassType(childElement.getAttribute("name"));
+	}
+
+	applyFilterName(name){
+		this.deselectAllChildren();
+		this.children[name].classList.add("filter-selected");
+		this.filterClassType(name);
+	}
+
+	deselectAllChildren(){
+		for (var i in this.children){
+			this.children[i].classList.remove("filter-selected");
+		}
+	}
+
+	filterClassType(type){
+		var elements = document.getElementsByClassName(this.filterClass);
+		for (var i = 0; i < elements.length; i++){
+			var e = elements[i];
+			if (type == "" || e.getAttribute("filter")?.split(' ').includes(type)){
+				e.style.display = "";
+			} else {
+				e.style.display = "none";
+			}
+		}
+	}
 }
